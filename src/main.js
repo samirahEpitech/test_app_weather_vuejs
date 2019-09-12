c9343497ec4f288291f5d70e45054c0eba432fe9
Sam <samirah.tachet@epitech.eu>
2019-09-12 11:53:36 +0200

    vueexo2_weatherapp

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import moment from 'moment'
+import VueWeatherWidget from 'vue-weather-widget'
+
+Vue.prototype.moment = moment
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App/>',
+  data: {
+    selected: ''
+  }
+})
+
+window.vm = new Vue({
+  el: '#app',
+  components: {
+    'weather': VueWeatherWidget
+  }
+})
